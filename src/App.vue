bdefeb089c5a1dc2843c30b0c0a0da5a83ca1285
Invoker_liu <liu54875487@163.com>
2018-05-12 21:32:43 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <HelloWorld/>
+    <todo-list v-for="(todoItem,index) in items" v-bind:item="todoItem" v-bind:key="index"></todo-list>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld'
+  import TodoList from "./components/TodoList";
+
+  export default {
+    name: 'App',
+    components: {
+      TodoList,
+      HelloWorld
+    },
+    data() {
+      return {
+        items: ['angular', 'vue', 'react']
+      }
+    }
+  }
+</script>
+A
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>{{item}}</li>
+</template>
+
+<script>
+  export default {
+    props: ['item'],
+    name: "TodoList"
+  }
+</script>
+
+<style scoped>
+
+</style>
